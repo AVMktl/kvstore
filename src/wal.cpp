0b0697e91bba8a2d687df3463b1c600178d0da4e
AVMktl <lakshyaguptaavm@gmail.com>
2026-09-06 13:17:02 +0000

    Recovery from partial writes

--- src/wal.cpp
+++ src/wal.cpp
@@ -11,25 +11,36 @@ void append(Record rcd, int fd){
 
 void recover(unordered_map<string, string> &mp){
     ifstream inFile("wal.bin", ios::binary);
-    if(inFile.is_open()){
+    if(!inFile.is_open()){
        return;
     }
     Operation operation; 
     uint32_t key_size, value_size;
+    streampos last_valid_pos = 0; 
 
     while(inFile.read(reinterpret_cast<char*>(&operation), sizeof(operation))){
-        inFile.read(reinterpret_cast<char*>(&key_size), sizeof(key_size));
-        inFile.read(reinterpret_cast<char*>(&value_size), sizeof(value_size));
+        if(!inFile.read(reinterpret_cast<char*>(&key_size), sizeof(key_size))) break;
+        if(!inFile.read(reinterpret_cast<char*>(&value_size), sizeof(value_size))) break;
 
         string key(key_size, '\0');
         string value(value_size, '\0');
-        inFile.read(reinterpret_cast<char*>(key.data()), key_size);
-        inFile.read(reinterpret_cast<char*>(value.data()), value_size);
+        if(!inFile.read(reinterpret_cast<char*>(key.data()), key_size)) break;
+        if(value_size > 0 && !inFile.read(reinterpret_cast<char*>(value.data()), value_size)) break;
 
         if(operation == Operation::Put){
             mp[key] = value;
         }else if(operation == Operation::Delete){
             mp.erase(key);
         }
+
+        last_valid_pos = inFile.tellg();
+    }
+
+    inFile.close();
+
+    int fd = open("wal.bin", O_RDWR);
+    if(fd >= 0){
+        ftruncate(fd, last_valid_pos);
+        close(fd);
     }
 }
